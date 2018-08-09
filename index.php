2b03202db06b0c0cfceff9a114f374c31acb4821
Arnaud63000 <40628189+Arnaud63000@users.noreply.github.com>
2018-08-10 01:19:06 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="Copyright" content="Ecliptique">
+    <meta name="description" content="Orienté sur l'univer du jeux video, parcours les section et trouve le jeu qui te passionne">
+    <meta http-equiv="Content-Language" content="fr">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Accueil</title>
+</head>
+<body>
+    <!-- NavBar -->
+        <div class="container-fluid navbar">
+            <nav class="row">
+              <a href="index.php">Accueil</a>
+              <a href="about.php">Contact</a>
+              <a href="profil.php">Profil</a>
+              <a href="register.php">S'enregistrer</a>
+              <a href="login.php">Connexion</a>
+              <a href="logout.php">Deconnexion</a>
+            </nav>
+        </div>
+    
+</body>
+</html>
